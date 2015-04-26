cfde1974686a96a2d2335f662eb2f2a18888ba9a
Aidi Stan <aidistan@live.com>
2015-04-26 11:48:08 +0800

    Implement gravatar-fix

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 为墙内的 WordPress 站点而生，目前已集成下列插件/功能：
 
-1. [zjhzxhz/google-font-fix](https://github.com/zjhzxhz/google-font-fix) : Google 字体服务替换
+1. [zjhzxhz/google-font-fix](https://github.com/zjhzxhz/google-font-fix) : 通过 360 代理获取 Google 字体
+2. gravatar-fix : 通过 https 访问 Gravatar 头像
+  - [Gravatar 头像被墙及解决方案](http://www.wpdaxue.com/gravatar-is-blocked.html)
--- insideGFW.php
+++ insideGFW.php
@@ -7,7 +7,7 @@ Plugin Name: insideGFW
 Plugin URI: https://github.com/aidistan/wp-insideGFW
 Description: 为墙内的 WordPress 站点而生
 Author: Aidi Stan
-Version: 0.1
+Version: 0.2
 Author URI: http://github.com/aidistan/
 
 *******************************************************************************
@@ -16,5 +16,6 @@ Author URI: http://github.com/aidistan/
 
 define('BASE_PATH', dirname(__FILE__));
 require BASE_PATH . '/google-font-fix.php';
+require BASE_PATH . '/gravatar-fix.php';
 
 ?>
